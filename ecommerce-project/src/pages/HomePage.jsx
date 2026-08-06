1920a303fcbf901b67ae913a6023bb9ac31e608e
Pasang Tamang <pasanghilp@gmail.com>
2026-08-06 10:54:15 +0545

    Use data from Backend to generate Html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../Components/Header";
-import { products } from "../../starting project/data/products";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 export function HomePage() {
-    axios.get("http://localhost:3000/api/products").then((response) => {
-        console.log(response.data);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/products").then((response) => {
+            setProducts(response.data);
+        });
     });
 
     return (
