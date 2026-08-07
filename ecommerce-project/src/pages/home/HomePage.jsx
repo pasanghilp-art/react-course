2a912fcb98b913ed62d3575a72ba03821ab15b5c
Pasang Tamang <pasanghilp@gmail.com>
2026-08-07 11:04:21 +0545

    Refactor HomePage component and move styles to new HomePage.css; create ProductsGrid component for product display

---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "../../Components/Header";
+import { ProductsGrid } from "./ProductsGrid";
+import "./HomePage.css";
+
+export function HomePage({ cart }) {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("/api/products").then((response) => {
+            setProducts(response.data);
+        });
+    }, []);
+
+    return (
+        <>
+            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+            <title>Ecommerce Website</title>
+
+            <Header cart={cart} />
+
+            <div className="home-page">
+                <ProductsGrid products={products} />
+            </div>
+        </>
+    );
+}
