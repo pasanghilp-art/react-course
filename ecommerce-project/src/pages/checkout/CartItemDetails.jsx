44a288521d98c26a13968fc7e9f6f77632daa908
Pasang Tamang <pasanghilp@gmail.com>
2026-08-08 19:48:55 +0545

    Move cartItem details  into new component

---
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -0,0 +1,27 @@
+import { priceFormater } from "../../utils/money";
+
+export function CartItemDetails({ cartItem }) {
+    return (
+        <>
+            <img className="product-image" src={cartItem.product.image} />
+
+            <div className="cart-item-details">
+                <div className="product-name">{cartItem.product.name}</div>
+                <div className="product-price">
+                    {priceFormater(cartItem.product.priceCents)}
+                </div>
+                <div className="product-quantity">
+                    <span>
+                        Quantity: <span className="quantity-label">2</span>
+                    </span>
+                    <span className="update-quantity-link link-primary">
+                        Update
+                    </span>
+                    <span className="delete-quantity-link link-primary">
+                        Delete
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+}
