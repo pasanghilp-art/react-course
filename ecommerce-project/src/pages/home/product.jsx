95bd2b6b66e62acabaa84110425059ca603a67e0
Pasang Tamang <pasanghilp@gmail.com>
2026-08-11 20:07:05 +0545

    Made added message pop up

--- ecommerce-project/src/pages/home/product.jsx
+++ ecommerce-project/src/pages/home/product.jsx
@@ -5,6 +5,7 @@ import CheckmarkIcon from "../../assets/images/icons/checkmark.png";
 
 export function Product({ product, loadCart }) {
     const [quantity, setQuantity] = useState(1);
+    const [showAdded, setShowAdded] = useState(false);
 
     const addToCart = async () => {
         await axios.post("/api/cart-items", {
@@ -12,6 +13,12 @@ export function Product({ product, loadCart }) {
             quantity,
         });
         await loadCart();
+
+        setShowAdded(true);
+
+        setTimeout(() => {
+            setShowAdded(false);
+        }, 2000);
     };
 
     const selectQuantity = (event) => {
@@ -60,7 +67,12 @@ export function Product({ product, loadCart }) {
 
             <div className="product-spacer"></div>
 
-            <div className="added-to-cart">
+            <div
+                className="added-to-cart"
+                style={{
+                    opacity: showAdded ? 1 : 0,
+                }}
+            >
                 <img src={CheckmarkIcon} />
                 Added
             </div>
