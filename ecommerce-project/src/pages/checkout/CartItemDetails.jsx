3b7dd19305dd153217395dbaa52ca2068781b960
Pasang Tamang <pasanghilp@gmail.com>
2026-08-12 19:16:38 +0545

    added functionality to the update button

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -4,9 +4,14 @@ import { priceFormater } from "../../utils/money";
 
 export function CartItemDetails({ cartItem, loadCart }) {
     const [textInput, setTextInput] = useState(false);
+    const [quantity, setQuantity] = useState(cartItem.quantity);
 
-    const updateCartItem = () => {
+    const updateCartItem = async () => {
         if (textInput) {
+            await axios.put(`/api/cart-items/${cartItem.product.id}`, {
+                quantity: Number(quantity),
+            });
+            await loadCart();
             setTextInput(false);
         } else {
             setTextInput(true);
@@ -16,6 +21,10 @@ export function CartItemDetails({ cartItem, loadCart }) {
     const deleteCartItem = async () => {
         await axios.delete(`/api/cart-items/${cartItem.productId}`);
         await loadCart();
+    };
+
+    const textUpdater = (event) => {
+        setQuantity(event.target.value);
     };
     return (
         <>
@@ -30,7 +39,12 @@ export function CartItemDetails({ cartItem, loadCart }) {
                     <span>
                         Quantity:
                         {textInput ? (
-                            <input type="text" className="textBox" />
+                            <input
+                                type="text"
+                                className="textBox"
+                                value={quantity}
+                                onChange={textUpdater}
+                            />
                         ) : (
                             <span className="quantity-label">
                                 {cartItem.quantity}
